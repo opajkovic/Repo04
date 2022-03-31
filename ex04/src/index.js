6b9ddc2a004eb5cf71911b065356d5f781634b3c
opajkovic <oljaolja170@gmail.com>
2022-03-31 20:29:09 +0200

    added ex04

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,18 +1,16 @@
 function main() {
-    var myDecimal= 8.2;
-    var  myDecimalE= 0.00254;
-  
-    myDecimal.;
-    myDecimalE.toExponential();
-  
-    return {
-      myDecimal,
-      myDecimalE
-    };
-    
-  }
-  
-  console.log(main());
-  
-  
-  module.exports = main;
+  var myDecimal = 8.2;
+  var myDecimalE = 2.54e-3;
+
+  myDecimal = myDecimal.toPrecision(3);
+  myDecimalE = myDecimalE.toPrecision();
+
+  return {
+    myDecimal,
+    myDecimalE
+  };
+
+}
+
+console.log(main());
+module.exports = main;
